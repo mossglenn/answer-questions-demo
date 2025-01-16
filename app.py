8c3ff98a598eef7da4df32187b632fa9490b1fca
Amos Glenn <amos@amosglenn.com>
2025-01-16 12:39:10 -0500

    icons in png

--- app.py
+++ app.py
@@ -333,8 +333,8 @@ with gr.Blocks(css_paths="customStyles.css") as demo:
     gr.HTML("<h4 style='text-align:center;'>NB: This demo runs on minimal "
             "resources; getting the response may take several seconds</h4>")
 
-    with gr.Group():
-        gr.HTML("<img src='information-svgrepo-com.svg' />")
+    with gr.Group(elem_classes=["group_question"]):
+        gr.HTML("<img src='information-svgrepo-com.png' />")
         question_box = gr.HTML(
             value="<div class='initial-text'>Selecting a question...</div>"
         )
