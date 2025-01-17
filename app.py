33750e7889366df458e106becf1de6b5835fcc90
Amos Glenn <amos@amosglenn.com>
2025-01-16 22:19:00 -0500

    less padding

--- app.py
+++ app.py
@@ -349,9 +349,10 @@ with gr.Blocks(css_paths="customStyles.css") as demo:
                     )
         with gr.Column(elem_classes=['heading-row-1']):
             gr.HTML("<div class='tester-text'>"
-                    "Check out the bottom of this page to read "
+                    "<h4>Instructions for Demo Testers</h4>"
+                    "<p>Check out the bottom of this page to read "
                     "<a href='#discussion'>How does this work?</a>"
-                    "<div class='nota-bena'><b>Please Note</b>: This demo "
+                    "<div class='nota-bena'><p><b>Please Note</b>: This demo "
                     "runs on minimal (mostly free) resources.<br />It may "
                     "take several seconds to respond to a submission with "
                     "feedback. </div>"
